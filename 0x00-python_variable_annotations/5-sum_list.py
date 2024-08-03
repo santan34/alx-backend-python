550385a00567f4c1b1ebebeb2133bf698a24a1a5
St <aaronndas@gmail.com>
2024-08-03 02:16:16 +0200

    documented 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""annoted summing function"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """the function it self"""
     sum: float = 0
     for i in input_list:
         sum = sum + i
